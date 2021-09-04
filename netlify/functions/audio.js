2b05f1d56fc0301dd7357af03db3d9414823c60c
Joanna Lin <linjoanna31@gmail.com>
2021-09-04 14:22:17 +1000

    Added more advanced text-to-speech pronunciations using Google APIs

---
+++ netlify/functions/audio.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+exports.handler = async(event, context) => {
+    try {
+        const response = await axios.post("https://texttospeech.googleapis.com/v1/text:synthesize?key=" + process.env.TTS_API_KEY, {
+            "audioConfig": {
+                "audioEncoding": "LINEAR16",
+                "pitch": 0.1,
+                "speakingRate": 0.85
+            },
+            "input": {
+                "text": JSON.parse(event.body).word
+            },
+            "voice": {
+                "languageCode": "en-AU",
+                "name": "en-AU-Wavenet-A"
+            }
+        })
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data)
+        }
+    } catch (error) {
+        return {
+            statusCode: 404,
+            body: error.toString(),
+        }
+    }
+}
--- src/components/WordBox.js
+++ src/components/WordBox.js
@@ -1,5 +1,7 @@
+import { useState, useEffect, useRef } from 'react';
 import { makeStyles, Card, IconButton, Typography } from '@material-ui/core'
 import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+import CircularProgress from '@material-ui/core/CircularProgress';
 const useStyles = makeStyles({
     wordCard: {
         margin: '0.5rem',
@@ -8,23 +10,45 @@ const useStyles = makeStyles({
         padding: '0.5rem 0.4rem 0.5rem 1.2rem',
         justifyContent: 'space-between',
         alignItems: 'center'
+    },
+    circularProgress: {
+        padding: '1rem'
     }
 })
 const WordBox = ({word}) => {
+    const audio = useRef();
     const styles = useStyles();
-    var msg = new SpeechSynthesisUtterance(word);
-    msg.volume = 1; // 0 to 1
-    msg.pitch = 1;
-    msg.rate = 0.75; // 0.1 to 10
-    msg.lang = 'en-AU';
+    const [ requestComplete, setRequestComplete ] = useState(false);
+    useEffect(() => {
+        fetch('/.netlify/functions/audio', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({word})
+        })
+        .then((res) => { 
+            return res.json();
+        })
+        .then((res) => {
+            audio.current = new Audio("data:audio/wav;base64," + res.audioContent)
+        })
+        .then(() => {
+            setRequestComplete(true);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }, [])
     return (
         <Card className={styles.wordCard}>
             <Typography variant='h6'>{word}</Typography>
-            <IconButton
-                onClick={() => window.speechSynthesis.speak(msg)}
-            >
-                <VolumeUpIcon fontSize='large'/>
-            </IconButton>
+            {requestComplete ?
+                <IconButton onClick={() => audio.current.play()}>
+                    <VolumeUpIcon fontSize='large'/>
+                </IconButton>:
+                <CircularProgress fontSize='large' />
+            }
         </Card>
     )
 }
